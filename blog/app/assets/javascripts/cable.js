13b46d908f33a01255e0c0055e39db7339dab015
Ty Enn <38800031+webdev719@users.noreply.github.com>
2018-12-11 11:26:26 -0500

    Blog App - Built using Ruby on Rails

---
+++ blog/app/assets/javascripts/cable.js
@@ -0,0 +1,8 @@
+// Action Cable provides the framework to deal with WebSockets in Rails
+
+(function() {
+  this.App || (this.App = {});
+
+  App.cable = ActionCable.createConsumer();
+
+}).call(this);
