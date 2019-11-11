9c8a28372a4b9c984a6e9d4a2e4ee6c344776d2b
Tyler Nilsson <tydev719@gmail.com>
2019-11-11 11:09:04 -0500

    removed 'blog' Ruby app due to certain gem security issues

--- blog/app/assets/javascripts/cable.js
+++
@@ -1,8 +0,0 @@
-// Action Cable provides the framework to deal with WebSockets in Rails
-
-(function() {
-  this.App || (this.App = {});
-
-  App.cable = ActionCable.createConsumer();
-
-}).call(this);
